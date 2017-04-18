9ae83c6d60aa8b0cebb4ce5bbb4159c990fb72d9
Alexandra Difuchina <sashadif@gmail.com>
2017-04-18 21:36:39 +0000

    first comit

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,11 @@
+class HomeController < ApplicationController
+    def index
+        render 'index.html.haml'
+    end
+    
+    def about; end
+
+    def how_it_works; end
+
+    def faq; end
+end
